5170638f6f7b55e9451ef7a993baf7f71f012b45
Nandana-0308 <ornandana03@gmail.com>
2026-05-14 23:40:10 +0530

    Added email feature for contact form

--- portfolio_backend/src/main/java/com/example/sahal_portfolio_backend/Services/EmailService.java
+++ portfolio_backend/src/main/java/com/example/sahal_portfolio_backend/Services/EmailService.java
@@ -1,6 +1,7 @@
 package com.example.sahal_portfolio_backend.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,21 +12,51 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${client.name}")
+    private String clientName;
+
+    @Value("${MAIL_USERNAME}")
+    private String ownerEmail;
+
     public void sendMail(String name, String email, String message) {
 
-        SimpleMailMessage mail = new SimpleMailMessage();
+        // =========================
+        // Mail to You
+        // =========================
 
-        // Your Gmail Address
-        mail.setTo("yourgmail@gmail.com");
+        SimpleMailMessage ownerMail = new SimpleMailMessage();
 
-        mail.setSubject("New Portfolio Contact Message");
+        ownerMail.setTo(ownerEmail);
 
-        mail.setText(
-                "Name: " + name + "\n" +
-                        "Email: " + email + "\n\n" +
-                        "Message:\n" + message
+        ownerMail.setSubject("New Portfolio Contact Message from "+name);
+
+        ownerMail.setText(
+                "Name : " + name + "\n" +
+                        "Email : " + email + "\n\n" +
+                        "Message :\n" + message
         );
 
-        mailSender.send(mail);
+        mailSender.send(ownerMail);
+
+
+        // =========================
+        // Confirmation Mail to Client
+        // =========================
+
+        SimpleMailMessage clientMail = new SimpleMailMessage();
+
+        clientMail.setTo(email);
+
+        clientMail.setSubject("Message Received Successfully");
+
+        clientMail.setText(
+                "Hi " + name + ",\n\n" +
+                        "Thank you for contacting me through my portfolio website.\n\n" +
+                        "I have received your message and will get back to you soon.\n\n" +
+                        "Regards,\n" +
+                        clientName
+        );
+
+        mailSender.send(clientMail);
     }
 }
