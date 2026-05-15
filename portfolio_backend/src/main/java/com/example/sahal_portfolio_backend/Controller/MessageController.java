e59294dcf16b9ac7cddf9c120e7332d56054d5c8
Nandana-0308 <ornandana03@gmail.com>
2026-05-15 20:20:13 +0530

    email problem solved

--- portfolio_backend/src/main/java/com/example/sahal_portfolio_backend/Controller/MessageController.java
+++ portfolio_backend/src/main/java/com/example/sahal_portfolio_backend/Controller/MessageController.java
@@ -24,19 +24,19 @@ public class MessageController {
         // Save in DB
         Message savedMessage = service.saveMessage(message);
 
-//        // Try sending email
-//        try {
-//
-//            emailService.sendMail(
-//                    message.getName(),
-//                    message.getEmail(),
-//                    message.getMessage()
-//            );
-//
-//        } catch (Exception e) {
-//
-//            System.out.println("Email sending failed: " + e.getMessage());
-//        }
+        // Try sending email
+        try {
+
+            emailService.sendMail(
+                    message.getName(),
+                    message.getEmail(),
+                    message.getMessage()
+            );
+
+        } catch (Exception e) {
+
+            System.out.println("Email sending failed: " + e.getMessage());
+        }
 
         return ResponseEntity.ok(savedMessage);
     }
